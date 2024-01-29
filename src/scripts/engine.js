5cf8d66539bd7b2b2a3e01ca1eb0a9fb2a293c14
HedroPedro <pedrohenrique.oliveira119@gmail.com>
2024-01-29 10:14:17 -0300

    criação das cartas

---
+++ src/scripts/engine.js
@@ -0,0 +1,26 @@
+const emojis = ['🥹','🥹',
+    '😎','😎','😡','😡','🤡','🤡'];
+let openCards = [];
+
+let shuffleEmojis = emojis.sort(()=>{return (Math.random() > 0.5) ? 2: -1});
+
+for(let i=0; i < emojis.length; i++){
+    let box = document.createElement("div");
+    box.className = "item";
+    box.innerHTML = shuffleEmojis[i];
+    box.onclick = handleClick;
+    document.querySelector(".game").appendChild(box);
+}
+
+function handleClick(){
+    if(openCards.length < 2){
+        this.classList.add("boxOpen");
+        openCards.push(this);
+    }
+
+    if(openCards.length === 2){
+        setTimeout(checkMatch, 500);
+    }
+}
+
+function checkMatch(){}
